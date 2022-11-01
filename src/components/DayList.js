716974e2c780c0e6e8776005e1cb72b921f58c3b
Kyrylo <kharchenkokyrylo@gmail.com>
2022-11-01 04:05:46 -0700

    Finish building the Day List

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,5 +3,15 @@ import "components/DayListItem.scss";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  return <ul></ul>;
+  const { days, setDay } = props;
+  const parsedDays = days.map((day) => (
+    <DayListItem
+      {...day}
+      key={day.id}
+      onClick={setDay}
+      selected={day.name === props.day}
+    />
+  ));
+
+  return <ul>{parsedDays}</ul>;
 }
