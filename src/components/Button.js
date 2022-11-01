9c362ad7c52dab33307c52207491b386b24e843e
Kyrylo <kharchenkokyrylo@gmail.com>
2022-11-01 01:06:06 -0700

    add button states and classes. No bugs

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,17 @@
 import React from "react";
-
 import "components/Button.scss";
+import classNames from "classnames";
 
 export default function Button(props) {
-   return <></>;
+  const { children, confirm, danger, disabled, onClick } = props;
+  const buttonClass = classNames("button ", {
+    "button--confirm": confirm,
+    "button--danger": danger,
+  });
+
+  return (
+    <button className={buttonClass} disabled={disabled} onClick={onClick}>
+      {children}
+    </button>
+  );
 }
