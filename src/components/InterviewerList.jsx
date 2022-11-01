e6da8ec86a4e4785ee5f8b523b69a425ff61d52c
Kyrylo <kharchenkokyrylo@gmail.com>
2022-11-01 08:27:58 -0700

    Create Interviewer List Item with story, scss, jsx

---
+++ src/components/InterviewerList.jsx
@@ -0,0 +1,1 @@
+import "components/InterviewerListItem.scss";
---
+++ src/components/InterviewerListItem.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "components/InterviewerListItem.scss";
+import classNames from "classnames";
+
+export default function InterviewerListItem(props) {
+  const { id, name, avatar, selected, setInterviewer } = props;
+  const interviewerListItemClass = classNames(
+    "interviewers__item", {
+    "interviewers__item--selected": selected
+  }
+  );
+  return (
+    <li
+      className={interviewerListItemClass}
+      key={id}
+      selected={selected}
+      onClick={() => setInterviewer(id)}
+    >
+      <img
+        className="interviewers__item-image"
+        src={avatar}
+        alt={name}
+      />
+      {selected ? name : ""}
+    </li>
+  );
+}
+
+// const interviewer = {
+//   id: 1,
+//   name: "Sylvia Palmer",
+//   avatar: "https://i.imgur.com/LpaY82x.png"
+// };
