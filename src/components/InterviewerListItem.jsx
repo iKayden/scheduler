d6a4c75d038a53623b5739ad772055572f02d4fb
Kyrylo <kharchenkokyrylo@gmail.com>
2022-11-01 16:14:44 -0700

    refactor of interview components. Prop drilling

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -3,30 +3,24 @@ import "components/InterviewerListItem.scss";
 import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
-  const { id, name, avatar, selected, setInterviewer } = props;
-  const interviewerListItemClass = classNames(
+  const { name, avatar, selected, setInterviewer } = props;
+  const interviewerClass = classNames(
     "interviewers__item", {
     "interviewers__item--selected": selected
   }
   );
   return (
     <li
-      className={interviewerListItemClass}
+      className={interviewerClass}
       selected={selected}
-      onClick={() => setInterviewer(id)}
+      onClick={setInterviewer}
     >
       <img
         className="interviewers__item-image"
         src={avatar}
         alt={name}
       />
-      {selected ? name : ""}
+      {selected && name}
     </li>
   );
 }
-
-// const interviewer = {
-//   id: 1,
-//   name: "Sylvia Palmer",
-//   avatar: "https://i.imgur.com/LpaY82x.png"
-// };
