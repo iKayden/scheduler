40e281564b209f21e7bec2407badb75f42f5a836
Kyrylo <kharchenkokyrylo@gmail.com>
2022-11-02 22:19:03 -0700

    add EMPTY and SHOW views for Appointment

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -4,22 +4,27 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
 
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
 
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {
-        props.interview ?
-          <Show
-            student={props.interview.student}
-            interviewer={props.interview.interviewer}
-          /> : <Empty />
-      }
-
-
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
     </article>
   );
 }
