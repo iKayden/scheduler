ff53ce0f27f1f1d14ef7428045ee0254d34ad224
Kyrylo <kharchenkokyrylo@gmail.com>
2022-11-07 04:18:58 -0800

    Create Spots counting function, put it in a hook

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,6 +4,7 @@ import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
 
+
   const parsedDays = props.days.map((day) => (
     <DayListItem
       key={day.id}
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -13,6 +13,23 @@ export default function useApplicationData() {
     interviewers: {}
   });
 
+  function countTheSpotsLeft(state, appointments) {
+
+    return state.days.map((day) => { //only updates selected (the one in our "state.day") day
+      if (day.name === state.day) { //only checking values for the current day and then returning it back
+        return {
+          ...day, //all the info for current day
+          spots: day.appointments //adding spots based on appointments, looping trough them
+            .map((id) => (appointments[id])) //access to all appointments
+            .filter(({ interview }) => { //if the current appointment falsy (doesn't exist)
+              return !interview; //give back only falsy ones
+            }).length //give back the length of null interviews and use it as free spots
+        };
+      }
+      return day; //just returning every day without mutating it
+    });
+  }
+
   function bookInterview(id, interview) {
     const appointment = {
       ...state.appointments[id],
@@ -24,7 +41,11 @@ export default function useApplicationData() {
     };
     return axios.put(`/api/appointments/${id}`, appointment)
       .then(() => {
-        setState({ ...state, appointments });
+        setState({
+          ...state,
+          appointments,
+          days: countTheSpotsLeft(state, appointments)
+        });
       });
   }
 
@@ -40,7 +61,11 @@ export default function useApplicationData() {
 
     return axios.delete(`/api/appointments/${id}`)
       .then(() => {
-        setState({ ...state, appointments });
+        setState({
+          ...state,
+          appointments,
+          days: countTheSpotsLeft(state, appointments)
+        });
       });
   }
 
