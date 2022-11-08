c2a17c35c3e8768c775479f972e86b9d9d7d3bf4
Kyrylo <kharchenkokyrylo@gmail.com>
2022-11-08 07:41:26 -0800

    finish jest tests for application and appointment

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,9 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, queryByText, getByPlaceholderText } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, queryByText, getByPlaceholderText, getByDisplayValue } from "@testing-library/react";
+
+/* somewhere near the top */
+import axios from "axios";
 
 import Application from "components/Application";
 
@@ -56,4 +59,100 @@ describe("Application Component", () => {
     // check if Monday has "no spots remaining" after booking an interview
     expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
+
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    // 1. Render the Application.
+    const { container, debug } = render(<Application />);
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => queryByText(container, "Archie Cohen"));
+
+    // get the correct appointment
+    const appointment = getAllByTestId(container, "appointment")
+      .find(app => queryByText(app, "Archie Cohen"));
+
+    // 3. Click the "Delete" button on the booked appointment.
+    fireEvent.click(getByAltText(appointment, "Delete"));
+
+    // 4. Check that the confirmation message is shown.
+    expect(getByText(appointment, "Are you sure you would like to delete your appointment?")).toBeInTheDocument();
+
+    // 5. Click the "Confirm" button on the confirmation.
+    fireEvent.click(getByText(appointment, "Confirm"));
+
+    // 6. Check that the element with the text "Deleting" is displayed.
+    expect(getByText(appointment, "DELETING APPOINTMENT"));
+
+    // 7. Wait until the element with the "Add" button is displayed.
+    await waitForElement(() => getByAltText(appointment, "Add"));
+
+    // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+    const day = getAllByTestId(container, "day").find(day => queryByText(day, "Monday"));
+    expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
+  });
+
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same",
+    async () => {
+      // 1. Render the Application.
+      const { container, debug } = render(<Application />);
+      // 2. Wait until the text "Archie Cohen" is displayed. (render wait)
+      await waitForElement(() => queryByText(container, "Archie Cohen"));
+      // 3. get the test appointment
+      const appointment = getAllByTestId(container, "appointment")
+        .find(app => queryByText(app, "Archie Cohen"));
+      // 4. Click the Edit button
+      fireEvent.click(getByAltText(appointment, "Edit"));
+
+      // 5. Check the state of dom, and if we can see the app in EDIT mode
+      expect(getByText(appointment, "Save")).toBeInTheDocument();
+
+      // 6. Edit the appointment, name for example. 
+      fireEvent.change(getByDisplayValue(appointment, "Archie Cohen"), { target: { value: "Kayden" } });
+
+      // 7. Click on Save btn
+      fireEvent.click(getByText(appointment, "Save"));
+      // 8. Check the DOM status, mode must be SAVING
+      expect(getByText(appointment, "SAVING IN THE PROCESS")).toBeInTheDocument();
+
+      // 9. Wait for changes to take place and find the day of change
+      await waitForElement(() => getByText(appointment, "Kayden"));
+      const day = getAllByTestId(container, "day").find(day => queryByText(day, "Monday"));
+      // 10. Check if the amount of spots is the same as it was
+      expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
+    });
+
+  it("shows the save error when failing to save an appointment", async () => {
+    axios.put.mockRejectedValueOnce();
+    const { container, debug } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointment = getAllByTestId(container, "appointment")
+      .find(app => queryByText(app, "Archie Cohen"));
+
+    fireEvent.click(getByAltText(appointment, "Edit"));
+
+    expect(getByText(appointment, "Save")).toBeInTheDocument();
+
+    fireEvent.change(getByDisplayValue(appointment, "Archie Cohen"), { target: { value: "Kayden" } });
+
+    fireEvent.click(getByText(appointment, "Save"));
+    await waitForElement(() => getByText(appointment, "Could not save your appointment"));
+  });
+
+  it("shows the delete error when failing to delete an existing appointment", async () => {
+    axios.delete.mockRejectedValueOnce();
+    const { container, debug } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    const appointment = getAllByTestId(container, "appointment")
+      .find(app => queryByText(app, "Archie Cohen"));
+
+    fireEvent.click(getByAltText(appointment, "Delete"));
+    expect(getByText(appointment, "Are you sure you would like to delete your appointment?")).toBeInTheDocument();
+
+    fireEvent.click(getByText(appointment, "Confirm"));
+    expect(getByText(appointment, "DELETING APPOINTMENT")).toBeInTheDocument();
+    await waitForElement(() => getByText(appointment, "Could not delete your appointment"));
+    debug(appointment);
+
+  });
+
 });
--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,18 +1,9 @@
-/*
-  We are rendering `<Application />` down below, so we need React.createElement
-*/
 import React from "react";
 
-/*
-  We import our helper functions from the react-testing-library
-  The render function allows us to render Components
-*/
 import { render, cleanup } from "@testing-library/react";
 import Appointment from "components/Appointment";
+
 afterEach(cleanup);
-/*
-  We import the component that we are testing
-*/
 
 describe("Appointment", () => {
   it("renders without crashing", () => {
@@ -24,6 +15,5 @@ describe("Appointment", () => {
     fn(1, 2);
     expect(fn).toHaveReturnedWith(42);
   });
-
 
 });
